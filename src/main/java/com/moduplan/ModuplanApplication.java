31ab463de6fb7fbff85a243fcdcf694e755186f4
육재민 <rjsekawoals@naver.com>
2026-03-24 17:49:27 +0900

    ci: test

--- src/main/java/com/moduplan/ModuplanApplication.java
+++ src/main/java/com/moduplan/ModuplanApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ModuplanApplication {
-	// ci test
+
 	public static void main(String[] args) {
 		SpringApplication.run(ModuplanApplication.class, args);
 	}
